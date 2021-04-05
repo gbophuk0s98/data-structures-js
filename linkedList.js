4bfcc9d414a380185cbbd549918b281f76e5d755
Surta <pasha.surta@mail.ru>
2021-04-05 15:39:45 +0300

    updated binary tree js

--- linkedList.js
+++ linkedList.js
@@ -12,7 +12,7 @@ class BinaryTree {
     }
 }
 
-BinaryTree.prototype.add = function(value) {
+BinaryTree.prototype.add = function (value) {
     let node = new Node(value),
         currentNode = this.head
     if (!currentNode) {
@@ -28,13 +28,87 @@ BinaryTree.prototype.add = function(value) {
             currentNode = currentNode.right
         }
     }
-    
+
     if (value < currentNode.value) {
         currentNode.left = node
     }
     else {
         currentNode.right = node
     }
+}
+
+BinaryTree.prototype.findMin = function () {
+    let currentNode = this.head
+
+    while (currentNode.left)
+        currentNode = currentNode.left
+    console.log(currentNode.value)
+}
+
+BinaryTree.prototype.findMax = function () {
+    let currentNode = this.head
+
+    while (currentNode.right)
+        currentNode = currentNode.right
+    console.log(currentNode.value)
+}
+
+BinaryTree.prototype.find = function (value) {
+    let currentNode = this.head
+    while (currentNode.value !== value) {
+        if (value < currentNode.value)
+            currentNode = currentNode.left
+        else currentNode = currentNode.right
+        if (!currentNode) return null
+    }
+    return currentNode
+}
+
+BinaryTree.prototype.isPresent = function (value) {
+    let currentNode = this.head
+    while (currentNode) {
+        if (value === currentNode.value) return true
+        value < currentNode.value ?
+            currentNode = currentNode.left : currentNode = currentNode.right
+    }
+    return false
+}
+
+BinaryTree.prototype.findMinInSubTree = function (tree) {
+    let currentNode = tree
+    while (currentNode.left)
+        currentNode = currentNode.left
+    return currentNode
+}
+
+BinaryTree.prototype.remove = function (value) {
+    let currentNode = this.head,
+        prevNode = null,
+        nodeToDeleted = null,
+        rigthPartMinNode = null,
+        leftPartMinNode = null
+
+    while (currentNode) {
+        if (currentNode.value === value) {
+            nodeToDeleted = currentNode
+            break
+        }
+        if (value < currentNode.value) {
+            prevNode = currentNode
+            currentNode = currentNode.left
+        }
+        else {
+            prevNode = currentNode
+            currentNode = currentNode.right
+        }
+    }
+    rigthPartMinNode = this.findMinInSubTree(nodeToDeleted.right)
+
+    value < prevNode.value ?
+        prevNode.left = nodeToDeleted.right :
+        prevNode.right = nodeToDeleted.right
+
+    rigthPartMinNode.left = nodeToDeleted.left
 }
 
 const tree = new BinaryTree()
@@ -45,5 +119,19 @@ tree.add(30)
 tree.add(50)
 tree.add(90)
 tree.add(80)
+tree.add(9)
+tree.add(12)
+tree.add(29)
+tree.add(35)
+tree.add(44)
+tree.add(46)
+
+// tree.findMin()
+// tree.findMax()
+// console.log(tree.find(53120))
+// console.log(tree.isPresent(40))
+tree.remove(20)
+tree.remove(50)
+tree.add(43)
 
 console.log(tree)
